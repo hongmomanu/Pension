44db9b477cbe6761945f3f4883534ad33def4cc1
weipan789 <weipan789@163.com>
2014-03-31 13:10:18 +0800

    pw

--- web/js/views/pension/EvaluateLrInfo.js
+++ web/js/views/pension/EvaluateLrInfo.js
@@ -1,6 +1,8 @@
 define(function(){
     var fieldset=[
-        {filehtml:'info0',title:'养老人员基本信息'},
+        {filehtml:'text!views/pension/LrBasicInfo.htm',
+            filejs:'views/pension/LrBasicInfo',
+            title:'养老人员基本信息',refOther:true},
         {filehtml:'info1',title:'生活自理能力'},
         {filehtml:'info2',title:'经济条件'},
         {filehtml:'info3',title:'居住环境'},
@@ -16,22 +18,14 @@ define(function(){
     ]
     var a=function(){
 
-        $('#submit').bind('click',function(){
-            $('#pinggu').form('submit',{
-                url:'lr.do?model=hzyl.EvaluateLrInfo&eventName=save',
-                onSubmit: function(param){
-                    param.p1 = 'value1';
-                    param.p2 = 'value2';
-                },
-                success:function(data){
-                    $.messager.alert('Info', data, 'info');
-                }
-            });
-        })
 
         for(var i=0;i<fieldset.length;i++){
             var filehtml='text!views/pension/evaluatelrinfofieldset/'+fieldset[i].filehtml+".htm";
             var title=fieldset[i].title;
+            if(fieldset[i].refOther){
+                filehtml=fieldset[i].filehtml;
+            }
+
             (function(h,t){
                 require([h],function(htmlfile){
                     $('#EvaluateLrInfo').tabs('add', {
@@ -42,6 +36,53 @@ define(function(){
             })(filehtml,title)
 
         }
+        window.setTimeout(function(){
+            $('#ownerid').combogrid({
+                panelWidth:420,
+                url: 'ajax/getLrBasicInfo.jsp',
+                idField:'owerid',
+                textField:'owerid',
+                validType:'personid',
+                mode:'remote',
+                fitColumns:true,
+                pagination:true,
+                onBeforeLoad: function(param){
+                    var options = $('#ownerid').combogrid('options');
+                    if(param.q!=null){
+                        param.query=param.q;
+                    }
+
+                },
+                onClickRow:function(index,row){
+                    $('#pensionform').form('load',row);
+                },
+                columns:[[
+                    {field:'name',title:'姓名',width:60},
+                    {field:'peopleid',title:'id',width:80},
+                    {field:'place',title:'地址',width:30}
+                ]]
+            });
+        },5000);
+        $('#EvaluateLrInfo').tabs({
+            tools:[{
+                iconCls:'bigSaveBtn',
+                text:'保存',
+                handler:function(){
+                    $('#pensionform').form('submit',{
+                        url:'lr.do?model=hzyl.EvaluateLrInfo&eventName=save',
+                        onSubmit: function(param){
+                            param.name = 'xx';
+                            param.identityid = '341282198707084617';
+                            param.registration = '33333';
+                        },
+                        success:function(data){
+                            $.messager.alert('Info', data, 'info');
+                        }
+                    });
+                }
+            }]
+        })
+
     }
 
     return {
