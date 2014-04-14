c656d1a453d399a13557983f76d3ed0e6bdeca3f
weipan789 <weipan789@163.com>
2014-04-14 14:37:29 +0800

    pw

--- web/js/extLocation.js
+++ web/js/extLocation.js
@@ -1,6 +1,7 @@
 
 //extLocation="http://127.168.2.141/ext-4.2.1/";
-
-extEasyui="http://127.168.2.141/easyui/";
-extKindeditor="http://127.168.2.141/kindeditor/";
+serverUrl="http://192.168.2.141:8080/"
+extEasyui=serverUrl+"easyui/";
+extKindeditor=serverUrl+"kindeditor/";
+uploadBase=serverUrl+"jasper/"
 
