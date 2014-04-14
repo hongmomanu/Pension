e26fc15162c7b9e6ebcff33a7e4f3140679b74f4
weipan789 <weipan789@163.com>
2014-04-14 08:49:32 +0800

    pw

---
+++ src/Pension/model/pension/EvaluateLrInfo.java
@@ -0,0 +1,113 @@
+package Pension.model.pension;
+
+import Pension.business.entity.User;
+import Pension.common.CommonDbUtil;
+import Pension.common.ParameterUtil;
+import Pension.common.RtnType;
+import Pension.common.sys.audit.AuditBean;
+import Pension.common.sys.audit.AuditManager;
+import Pension.common.sys.audit.IMultilevelAudit;
+import Pension.model.Model;
+import net.sf.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EvaluateLrInfo extends Model implements IMultilevelAudit {
+
+
+    public String save(){
+        CommonDbUtil commonDbUtil=new CommonDbUtil();
+        Map map= ParameterUtil.toMap(this.getRequest());
+        String digest=this.getRequest().getParameter("name")+
+                this.getRequest().getParameter("identityid")+
+                this.getRequest().getParameter("registration");
+        int result=0;
+        Long id=commonDbUtil.getSequence("SEQ_T_NEEDASSESSMENT");
+        map.put("pg_id",id);
+        String tablename="t_needassessment";
+        User user=(User)this.getRequest().getSession().getAttribute("user");
+        AuditManager.addAudit(id,
+                this.getRequest().getParameter("functionid")
+                , tablename,
+                this.getClass().getName(), digest,
+                user.getLoginname(),user.getUsername(),user.getRegionid()
+                );
+        commonDbUtil.insertTableVales(map,"t_needassessmentsum");
+        result=commonDbUtil.insertTableVales(map,tablename);
+        return result>0? RtnType.SUCCESS:RtnType.FAILURE;
+    }
+    public String query(){
+        CommonDbUtil commonDbUtil=new CommonDbUtil();
+        Integer page=Integer.parseInt(this.getRequest().getParameter("page"));
+        Integer rows=Integer.parseInt(this.getRequest().getParameter("rows"));
+
+        Map map= new HashMap();
+        String sql="select o.name,o.identityid,o.birthd,o.gender,o.age,o.nation,o.address,o.type,o.registration" +
+                ",n.* from t_needassessment n,t_oldpeople o where o.lr_id=n.lr_id order by n.pg_id desc";
+        List list=commonDbUtil.query("SELECT * FROM (SELECT tt.*, ROWNUM ro FROM ("+sql+") tt WHERE ROWNUM <="+(page)*rows+") WHERE ro > "+(page-1)*rows);
+        int count=commonDbUtil.query(sql).size();
+        map.put("total",count);
+        map.put("rows",list);
+        return JSONObject.fromObject(map).toString();
+    }
+    public String queryById(){
+        CommonDbUtil commonDbUtil=new CommonDbUtil();
+        Integer id=Integer.parseInt(this.getRequest().getParameter("id"));
+
+        Map map= new HashMap();
+        String sql="SELECT t1.*,t2.* from t_needassessment t1,t_needassessmentsum t2 where t1.pg_id=t2.pg_id and t1.pg_id="+id;
+        List list=commonDbUtil.query(sql);
+        if(list.size()>0){
+            map=(Map)list.get(0);
+        }
+        return JSONObject.fromObject(map).toString();
+    }
+
+    public String queryPeople(){
+        CommonDbUtil commonDbUtil=new CommonDbUtil();
+        Integer id=Integer.parseInt(this.getRequest().getParameter("id"));
+
+        Map map= new HashMap();
+        String sql="select * from t_oldpeople where lr_id="+id;
+        List list=commonDbUtil.query(sql);
+        if(list.size()>0){
+            map=(Map)list.get(0);
+        }
+        return JSONObject.fromObject(map).toString();
+    }
+
+    /*审核成功后的回调
+    * 设置评估信息的状态为有效
+    * */
+    @Override
+    public Long audit(AuditBean auditBean) {
+        Long pg_id=Long.parseLong(auditBean.getTprkey());
+        CommonDbUtil dbUtil=new CommonDbUtil();
+        String sql="update t_needassessment set active='1' where pg_id="+pg_id;
+        Map currentAudit=auditBean.getCurrentAudit();
+        String comment=",";
+        String level=(String)currentAudit.get("aulevel");
+        if("1".equals(level)){
+            comment+=" shequcomment";
+        }else if("2".equals(level)){
+            comment+=" xiangzhencomment";
+        }else if("3".equals(level)){
+            comment+=" minzhengcomment";
+            dbUtil.execute(sql);
+        }
+
+        String sql2="";
+        if(null==currentAudit.get("audesc")||"null".equals(currentAudit.get("audesc"))){
+            sql2="update t_needassessmentsum set pingguenddate=sysdate where pg_id="+pg_id;
+        }else{
+            comment+="='"+currentAudit.get("audesc")+"' ";
+            sql2="update t_needassessmentsum set pingguenddate=sysdate "+comment+" where pg_id="+pg_id;
+        }
+        dbUtil.execute(sql2);
+        return null;
+    }
+}
---
+++ src/Pension/model/pension/PensionPeopleInfoEdit.java
@@ -0,0 +1,136 @@
+package Pension.model.pension;
+
+import Pension.common.CommonDbUtil;
+import Pension.common.ParameterUtil;
+import Pension.common.RtnType;
+import Pension.common.sys.audit.AuditBean;
+import Pension.common.sys.audit.IMultilevelAudit;
+import Pension.model.Model;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-3-20
+ * Time: 下午5:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class PensionPeopleInfoEdit extends Model implements IMultilevelAudit {
+    private HttpServletRequest request;
+    private Connection conn;
+
+
+
+  /*  public String save(){                 //老年基本信息保存方法
+        int result=0;
+        CommonDbUtil commonDbUtil=new CommonDbUtil(conn);
+
+        Map map= ParameterUtil.toMap(request);             //获取提交过来的结果
+
+        String gxmess = map.get("p1").toString();                  //获得老年关系人员数据
+        JSONArray ga = JSONArray.fromObject(gxmess);         //转换成JSON数据
+
+        String briefmessage = map.get("name").toString()+","+map.get("gender").toString()+",出生于"+map.get("birthd").toString()+",原籍"+map.get("registration").toString()+",现居住"+map.get("address").toString();
+
+
+        Long id=commonDbUtil.getSequence("seq_t_oldpeople");                //创建唯一标识
+        map.put("lr_id",id);                                                                                 //存放到老年基础信息表里
+
+        for(int i=0;i<ga.size();i++){                                                                     //循环将多个老年关系数据存入数据库
+           Map m = ParameterUtil.toMap(JSONObject.fromObject(ga.get(i)))  ;
+            m.put("lr_id",id) ;                                                                                      //将唯一标识存入关系表里
+            commonDbUtil.insertTableVales(m ," T_OLDSOCREL");                          //将数据存入数据库中
+        }
+
+
+
+        AuditManager.addAudit(id, "mHLcDiwTflgEshNKIiOV", "T_OLDPEOPLE",          //审核功能添加
+                this.getClass().getName(), briefmessage,                                       // + new Date().toString()
+                request.getSession().getAttribute("loginname").toString(),
+                request.getSession().getAttribute("username").toString(),
+                request.getSession().getAttribute("dvcode").toString()
+        );
+        //AuditManager.addAudit(id);
+        result=commonDbUtil.insertTableVales(map,"t_oldpeople");                     //将老年基础数据存入数据库中
+        return result>0? RtnType.SUCCESS:RtnType.FAILURE;
+    }*/
+
+    public String update(){                 //老年基本信息修改方法
+        int result=0;
+        CommonDbUtil commonDbUtil=new CommonDbUtil(conn);
+        String lr_id = request.getParameter("lr_id");             //获取唯一标识id
+
+        String delsql = "DELETE FROM T_OLDSOCREL WHERE lr_id = '"+lr_id+"'";       //删除原先的关系数据 sql
+        commonDbUtil.execute(delsql);                                                                                  // 执行sql语句
+
+        Map where=new HashMap();                                 //
+        where.put("lr_id",lr_id);                                            //将唯一标识存入条件中
+
+        Map map= ParameterUtil.toMap(request);             //获取传过来的数据
+
+        String gxmess = map.get("p1").toString();                   //获取老年关系数据
+        JSONArray ga = JSONArray.fromObject(gxmess);
+        for(int i=0;i<ga.size();i++){
+            Map mc = ParameterUtil.toMap(JSONObject.fromObject(ga.get(i)))  ;
+            mc.put("lr_id",lr_id);
+            commonDbUtil.insertTableVales(mc,"T_OLDSOCREL");                //将新数据插入进去
+        }
+
+        //String lr_id = request.getParameter("lr_id");
+        /*Long id=commonDbUtil.getSequence("seq_t_oldpeople");
+        map.put("id",id);*/
+       // Map where=new HashMap();
+        //where.put("lr_id",lr_id);
+
+        /*AuditManager.addAudit(id, "mHLcDiwTflgEshNKIiOV", "T_OLDPEOPLE",          //审核功能添加
+                this.getClass().getName(), "没有摘要信息" + new Date().toString(),
+                request.getSession().getAttribute("loginname").toString(),
+                request.getSession().getAttribute("username").toString(),
+                request.getSession().getAttribute("dvcode").toString()
+        );*/
+        //AuditManager.addAudit(id);
+        result=commonDbUtil.updateTableVales(map,"t_oldpeople",where);
+        return result>0? RtnType.SUCCESS:RtnType.FAILURE;
+    }
+
+    public String editLrbasicInfo() {
+        String lr_id=request.getParameter("lr_id");                                                 //获取选中行的主键
+        CommonDbUtil commonDbUtil=new CommonDbUtil(conn);
+        //StringBuffer sb=new StringBuffer();
+        //String node=request.getParameter(peopleid);
+        String sql="SELECT * FROM T_OLDPEOPLE WHERE lr_id='"+lr_id+"'";
+        List list=commonDbUtil.query(sql);                                                          //根据主键进行数据查询
+        Map map=new HashMap();
+        if(list.size()>0){
+            map=(Map)list.get(0);
+        }
+        return JSONObject.fromObject(map).toString();
+
+    }
+
+    public String setGxDate(){                                                                            //获取对应的成员关系数据
+        CommonDbUtil commonDbUtil=new CommonDbUtil(conn);
+        List<Map<String,Object>> gxlist = new ArrayList<Map<String,Object>>();
+        String lr_id = request.getParameter("lr_id");
+        String gxsql = "SELECT * FROM T_OLDSOCREL WHERE lr_id = "+lr_id;
+        gxlist = commonDbUtil.query(gxsql);
+        return JSONArray.fromObject(gxlist).toString();                                                   //return JSONArray.fromObject(commonDbUtil.query(sql)).toString()
+    }
+
+    @Override
+    public Long audit(AuditBean auditBean) {
+        Long lr_id=Long.parseLong(auditBean.getTprkey());
+        CommonDbUtil dbUtil=new CommonDbUtil();
+        String sql="update T_OLDPEOPLE set active='1' where lr_id="+lr_id;
+        dbUtil.execute(sql);
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
