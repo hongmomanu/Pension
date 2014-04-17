4b3a00a398f6a99617ab30bcb2b6590cf57b9c57
weipan789 <weipan789@163.com>
2014-04-17 11:57:40 +0800

    pw

---
+++ web/js/commonfuncs/Htmldb.js
@@ -0,0 +1,51 @@
+/*根据当前页面元素和easyui插件的值生成页面保存到后台*/
+
+define(function(){
+    var parse=function(local){
+        var selections='.easyui-combobox';
+        var eobj={};
+
+        local.find('textarea,:input').each(function(){    //获得元素input的value
+            if($(this).val()){
+                eobj[$(this).attr('name')]=$(this).val();
+            }
+        })
+
+        local.find(selections).each(function(){
+            var comboname=$(this).attr('comboname');
+            if(comboname){
+                eobj[comboname]=$(this).combo('getText');
+            }
+        })
+
+        var myclone=$(local).clone();
+        myclone.find(selections).each(function(){   //删除easyui样式
+            $(this).removeAttr('data-options').css('display','inline')
+                .removeClass('easyui-combobox').attr('comboname');
+        })
+        myclone.find('textarea,:input').each(function(){      //为input元素赋值
+            var name=$(this).attr('name');
+            var tagName=$(this).prop('tagName');
+            if(tagName=="INPUT"){
+                $(this).attr('value',eobj[name]);
+            }else if(tagName=="TEXTAREA"){
+                $(this).text(eobj[name]);
+            }
+
+            var comboname=$(this).attr('comboname')
+            if(comboname){
+                $(this).attr('value',eobj[comboname]);
+            }
+        })
+        myclone.find('span[class=combo]').remove();
+       /* $('#tabs').tabs('add',{
+            title:'原始页面',
+            content:myclone.html(),
+            closable:true
+        })*/
+        console.log(eobj)
+        return myclone.html();
+    }
+
+    return {parse:parse}
+})
